eea67d3e4cb175db432b4401a9e7d84de1fb545b
GigaVoltZX <91165917+GigaVoltZX@users.noreply.github.com>
2022-01-12 19:00:42 -0600

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,35 @@
+class Game {
+  constructor() {}
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  play() {
+   this.handleElements();
+   player.getPlayersInfo();
+  if (allPlayers!=undefined){
+   Image(track,0,-height*5,width,height*6);
+   drawSprites();
+   
+
+   }
+  }
+ }
